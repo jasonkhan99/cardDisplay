d808a881bec0d2dce2f2b18cd4f652759b3ceb2d
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-08 14:54:50 -0700

    create functions to combine suits and nums arrays into playing card names
    co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
   const cardDeck = [];
-  const suit = ["hearts", "clubs", "diamonds", "spades"];
+  const suits = ["hearts", "clubs", "diamonds", "spades"];
   const nums = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-
-
-
-
-
-
+  suits.forEach(function(suit) {
+    nums.forEach(function(num) {
+      cardDeck.push(" " + num + " of " + suit);
+    });
+  });
+  $("#cardDeck").text(cardDeck);
 });
